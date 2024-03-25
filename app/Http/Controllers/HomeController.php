3d78683c6d2d6d913d68dd6fb31450cb091a2792
dedy-gunkid <gunkiddedy@gmail.com>
2024-03-26 00:04:31 +0700

    update HomeController and navbar UI

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,18 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data['shell_count'] =\App\Models\Shell::count();
+		$data['shell_count_active'] =\App\Models\Shell::where('status', 'active')->count();
+		$data['shell_count_inactive'] =\App\Models\Shell::where('status', 'inactive')->count();
+		$data['vps_count'] =\App\Models\Vps::count();
+		$data['vps_count_active'] =\App\Models\Vps::where('status', 'active')->count();
+		$data['vps_count_inactive'] =\App\Models\Vps::where('status', 'inactive')->count();
+		$data['cpanel_count'] =\App\Models\Cpanel::count();
+		$data['cpanel_count_active'] =\App\Models\Cpanel::where('status', 'active')->count();
+		$data['cpanel_count_inactive'] =\App\Models\Cpanel::where('status', 'inactive')->count();
+
+        return view('home', [
+			'data' => $data
+		]);
     }
 }
--- resources/views/include/app.blade.php
+++ resources/views/include/app.blade.php
@@ -51,6 +51,37 @@
                         <a href="{{ route('user') }}" class="p-3 text-decoration-none text-light"><i class="fa fa-users fa-fw"></i> USERS</a>
                         {{-- <a href="{{ route('role') }}" class="p-3 text-decoration-none text-light"><i class="fa fa-gears fa-fw"></i> ROLES</a> --}}
                     </div>
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link text-light text-uppercase" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link text-light text-uppercase" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-light text-uppercase" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+    
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item text-light text-uppercase" href="{{ route('logout') }}"
+                                      onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+    
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
                 </div>
             </nav>
         </div>
